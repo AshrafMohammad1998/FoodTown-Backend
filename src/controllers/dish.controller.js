ff3206996a632e09aa02dd7fc3f2766d28e3f52a
MohammadAshra <133404331+MohammadAshra@users.noreply.github.com>
2024-07-14 13:33:15 +0530

    partner restaurant dishes add, update, delete

--- src/controllers/dish.controller.js
+++ src/controllers/dish.controller.js
@@ -5,7 +5,10 @@ const nodemailer = require("nodemailer");
 const fs = require("fs");
 const path = require("path");
 const Dish = require("../models/dish.model");
-const { uploadToCloudinary, deleteFromCloudinary } = require("../utils/cloudinary");
+const {
+  uploadToCloudinary,
+  deleteFromCloudinary,
+} = require("../utils/cloudinary");
 
 // ----------------------email section starts here-----------------------
 
@@ -128,7 +131,6 @@ const uploadDishImage = async (req, res) => {
   const { restaurantName, oldImgPublicId } = req.body;
 
   try {
-
     if (oldImgPublicId) {
       await deleteFromCloudinary(oldImgPublicId, "image");
     }
@@ -136,9 +138,7 @@ const uploadDishImage = async (req, res) => {
     const dishLocalImg = req.file;
 
     if (!dishLocalImg) {
-      return res
-        .status(400)
-        .json(new APIError(400, "Dish image is required"));
+      return res.status(400).json(new APIError(400, "Dish image is required"));
     }
 
     const cloudinaryImgData = await uploadToCloudinary({
@@ -159,26 +159,68 @@ const uploadDishImage = async (req, res) => {
       )
     );
   } catch (error) {
-    console.log(
-      "Dish Controller :: Upload Dish Image :: Error ",
-      error
-    );
+    console.log("Dish Controller :: Upload Dish Image :: Error ", error);
   }
 };
 
 const getDishes = async (req, res) => {
-  try{
+  try {
+    const { restaurantId } = req.params;
+    const dishes = await Dish.find({ restaurant: restaurantId });
 
-    const {restaurantId} = req.params
-    const dishes = await Dish.find({restaurant: restaurantId})
-    
-    return res.status(200).json(new APIResponse(200, dishes, "Dishes fetched Successfully."))
-  }catch(error){
-    console.log(
-      "Dish Controller :: Get Dishes :: Error ",
-      error
-    );
+    return res
+      .status(200)
+      .json(new APIResponse(200, dishes, "Dishes fetched Successfully."));
+  } catch (error) {
+    console.log("Dish Controller :: Get Dishes :: Error ", error);
   }
+};
+
+const deleteDish = async (req, res) => {
+  try {
+    const { dishId } = req.params;
+    const {imageId} = req.query
+
+    if (imageId) {
+      await deleteFromCloudinary(imageId, "image");
+    }
+
+    await Dish.findByIdAndDelete(dishId)
+    return res.status(200).json(new APIResponse(200, {}, "Dish item is deleted Successfully."))
+
+  } catch (error) {
+    console.log("Dish Controller :: Delete Dish :: Error:", error);
+  }
+};
+
+const updateDish = async (req, res) => {
+  const {dishId} = req.params
+  const {dishName, amount, description, productImgPublicId, producttImgUrl} = req.body 
+
+  try {
+
+    if (!dishName || !amount || !description ){
+      return res.status(400).json(new APIError(400, "All fields are mandatory"))
+    }
+    const updateDish = await Dish.findByIdAndUpdate(
+      dishId,
+      {
+        dishName,
+        amount,
+        description,
+        dishImage: {
+          url: producttImgUrl,
+          publicID: productImgPublicId
+        }
+      },
+      {new: true}
+    )
+
+    return res.status(200).json(new APIResponse(200, updateDish, "Dish updated Successfully."))
+  } catch (error) {
+    console.log("Dish Controller :: Edit Dish :: Error:", error);
+  }
+  
 }
 
-module.exports = { createDish, uploadDishImage,getDishes };
+module.exports = { createDish, uploadDishImage, getDishes, deleteDish, updateDish };
--- src/routes/dish.routes.js
+++ src/routes/dish.routes.js
@@ -1,7 +1,7 @@
 const { Router } = require("express");
 const verifyJWT = require("../middlewares/auth.middleware");
 const upload = require("../middlewares/multer.middleware");
-const { createDish, uploadDishImage,getDishes } = require("../controllers/dish.controller");
+const { createDish, uploadDishImage,getDishes,deleteDish,updateDish } = require("../controllers/dish.controller");
 
 
 const dishRouter = Router()
@@ -14,5 +14,7 @@ dishRouter
   .post(verifyJWT, upload.single("dishImg"), uploadDishImage);
 
   dishRouter.route("/getDishes/:restaurantId").get(getDishes)
+  dishRouter.route("/deleteDish/:dishId").delete(deleteDish)
+  dishRouter.route("/updateDish/:dishId").put(updateDish)
 
 module.exports = dishRouter
